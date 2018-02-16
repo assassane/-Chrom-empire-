96d46a744869d2e24f0daa1c734c062419a51113
assassane <assahimmi@gmail.com>
2018-02-16 19:37:37 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on("message", async message => {
   
   if(command === "ping") {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    //m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
   if(command === "say") {
