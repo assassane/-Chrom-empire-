c34b053834db7111e8879402775ca54a055ade3d
assassane <assahimmi@gmail.com>
2018-02-16 19:44:07 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,18 @@ client.on("ready", () => {
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  newUsers.set(member.id, member.user);
+
+  if (newUsers.size > 10) {
+    const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
+    const userlist = newUsers.map(u => u.toString()).join(" ");
+    defaultChannel.send("vtff connard, " + userlist);
+    newUsers.clear();
+  }
+});
+
 client.on("message", async message => {
   
   if(message.author.bot) return;
